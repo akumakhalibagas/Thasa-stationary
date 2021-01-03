2d4305f9c1cb05e58c38d8c9605bdd27931843c7
Sindu Andita <sinduandita@gmail.com>
2021-01-04 06:17:49 +0700

    e-commerce project ini

---
+++ repositories/users.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+class UserRepository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error('Creating a repositoy requires a filename');
+    }
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (err) {
+      fs.writeFileSync(this.filename, '[]');
+    }
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString('hex');
+  }
+
+  async getOneBy(filters) {
+    const records = await this.getAll();
+
+    for (let record of records) {
+      let found = true;
+      for (let key in filters) {
+        if (record[key] !== filters[key]) [(found = false)];
+      }
+      if (found) {
+        return record;
+      }
+    }
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter(record => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find(record => record.id === id);
+    if (!record) {
+      throw new Error(`Record with id ${id} didn't exist`);
+    }
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find(record => record.id === id);
+  }
+
+  async create(attrs) {
+    attrs.id = this.randomId();
+    const records = await this.getAll();
+    records.push(attrs);
+    await this.writeAll(records);
+  }
+
+  async writeAll(records) {
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  async getAll() {
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: 'utf8',
+      })
+    );
+  }
+}
+
+module.exports = new UserRepository('users.json');
