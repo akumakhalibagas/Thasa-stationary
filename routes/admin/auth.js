5ee88f4e1b51577f1fa2a2adda04a5ad671b8e30
Sindu Andita <sinduandita@gmail.com>
2021-01-05 00:57:54 +0700

    project restructuring

---
+++ routes/admin/auth.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { validationResult } = require('express-validator');
+const usersRepo = require('../../repositories/users');
+const signupTemplate = require('../../views/admin/auth/signup');
+const signinTemplate = require('../../views/admin/auth/signin');
+const {
+  requireEmail,
+  requirePassword,
+  requirePasswordConfirmation,
+} = require('./validators');
+
+const router = express.Router();
+
+router.get('/signup', (req, res) => {
+  res.send(signupTemplate({ req }));
+});
+
+router.post(
+  '/signup',
+  [requireEmail, requirePassword, requirePasswordConfirmation],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+      return res.send(signupTemplate({ req, errors }));
+    }
+    const { email, password, passwordConfirmation } = req.body;
+    const user = await usersRepo.create({ email, password });
+    req.session.userId = user.id;
+    res.send('Account Created');
+  }
+);
+
+router.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('You are logged out');
+});
+
+router.get('/signin', (req, res) => {
+  res.send(signinTemplate());
+});
+
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await usersRepo.getOneBy({ email });
+  if (!user) {
+    return res.send('Email not found');
+  }
+  const isValidPassword = await usersRepo.comparePassword(
+    user.password,
+    password
+  );
+  if (!isValidPassword) {
+    return res.send('Invalid password');
+  }
+  req.session.userId = user.id;
+  res.send('You are sign in');
+});
+
+module.exports = router;
