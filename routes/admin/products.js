e312807a43291360e7a10a735fb0eba2ffbaf5f9
Sindu Andita <sinduandita@gmail.com>
2021-01-06 14:02:11 +0700

    edit product routing, inputs validating, edit product page

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -4,6 +4,7 @@ const multer = require('multer');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productIndexTemplate = require('../../views/admin/products/index');
+const productEditTemplate = require('../../views/admin/products/edit');
 const { requireTitle, requirePrice } = require('./validators');
 const { handleErrors, requireAuth } = require('./middleware');
 
@@ -29,6 +30,39 @@ router.post(
     const image = req.file.buffer.toString('base64');
     const { title, price } = req.body;
     await productsRepo.create({ title, price, image });
+    res.redirect('/admin/products');
+  }
+);
+
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+  const product = await productsRepo.getOne(req.params.id);
+  if (!product) {
+    return res.send('Product not found');
+  }
+  res.send(productEditTemplate({ product }));
+});
+
+router.post(
+  '/admin/products/:id/edit',
+  requireAuth,
+  upload.single('image'),
+  [requireTitle, requirePrice],
+  handleErrors(productEditTemplate, async req => {
+    const product = await productsRepo.getOne(req.params.id);
+    return { product };
+  }),
+  async (req, res) => {
+    const changes = req.body;
+    if (req.file) {
+      changes.image = req.file.buffer.toString('base64');
+    }
+
+    try {
+      await productsRepo.update(req.params.id, changes);
+    } catch (err) {
+      return res.send('Could not find item');
+    }
+
     res.redirect('/admin/products');
   }
 );
